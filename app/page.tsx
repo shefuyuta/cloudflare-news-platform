051715b48b881587323506f07c2e00219a211150
shefuyuta <77376816+shefuyuta@users.noreply.github.com>
2026-05-17 09:34:20 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 // app/page.tsx
-import { getRequestContext } from "@cloudflare/next-on-pages";
+import { getRequestContext } from " @opennextjs/cloudflare";
 import { listArticles } from "@/lib/db";
 import { NewsList } from "@/components/news/NewsList";
 import type { Env } from "@/lib/types";
